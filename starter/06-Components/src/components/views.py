829a6bdf1d304b356f402f67ba265f1f1ee8eb95
thebjko <thebjko@gmail.com>
2023-11-30 13:17:13 +0900

    accordion

---
+++ starter/06-Components/src/components/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def accordion(request):
+    return render(request, 'components/accordion.html')
---
+++ starter/06-Components/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/templates/**/*.html",
+    "./src/components/templates/components/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
