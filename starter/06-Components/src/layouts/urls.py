948a3232a802e9bc99ab6a9c810e93c3ad340628
thebjko <thebjko@gmail.com>
2023-12-01 18:04:09 +0900

    begin app layout - email

--- starter/06-Components/src/layouts/urls.py
+++ starter/06-Components/src/layouts/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'layouts'
 urlpatterns = [
     path('hero/', views.hero, name='hero'),
+    path('email/', views.email, name='email'),
 ]
--- starter/06-Components/src/layouts/views.py
+++ starter/06-Components/src/layouts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def hero(request):
     return render(request, 'layouts/hero.html')
+
+
+def email(request):
+    return render(request, 'layouts/email.html')
