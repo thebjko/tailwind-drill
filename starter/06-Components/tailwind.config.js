b1a89c317600ce99b38e4811dff232317f9ae1f6
thebjko <thebjko@gmail.com>
2023-12-01 17:59:26 +0900

    update tailwind config

--- starter/06-Components/tailwind.config.js
+++ starter/06-Components/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/templates/**/*.html",
     "./src/components/templates/components/**/*.html",
+    "./src/layouts/templates/layouts/**/*.html",
   ],
   theme: {
     extend: {},
